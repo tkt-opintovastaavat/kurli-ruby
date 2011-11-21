8fd879d5c4070b385bb1d1955ddbe340bfeb6859
Sami Saada <sami.saada@enemy.fi>
2011-11-21 20:08:01 +0200

    implemented parser

--- lib/kurli/parser.rb
+++ lib/kurli/parser.rb
@@ -3,11 +3,29 @@ module Kurli
 
     def initialize(data_string)
       require 'json'
-      JSON.parse(data_string)
+      @json = JSON.parse(data_string)
     end
 
     def fetch_courses
-      []
+      @json.map{ |course_data| parse_course(course_data) }
+    end
+
+    private
+
+    def parse_course(data)
+      Kurli::Course.new(data["code"],
+                        parse_name(data["name"]),
+                        data["credits"],
+                        data["level"]
+                       )
+    end
+
+    def parse_name(name_data)
+      parsed_names = {}
+      name_data.each do |key, value|
+        parsed_names[key.to_sym] = value
+      end
+      parsed_names
     end
 
   end
